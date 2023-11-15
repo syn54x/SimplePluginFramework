f93e032bb84e5ad77e78fa22cf0024491a5b3e16
Myg0t <myg0tm3dia@gmail.com>
2023-11-14 23:37:14 -0500

    spf deps

--- src/spf/PluginMount.py
+++ src/spf/PluginMount.py
@@ -1,15 +1,23 @@
 from abc import ABCMeta
 from typing import List, Any
 
-import pkg_resources
+try:
+    from importlib.metadata import entry_points
+
+    USE_LEGACY = False
+
+except ImportError:
+    import pkg_resources
+
+    USE_LEGACY = True
 
 
 class PluginMount(ABCMeta):
     REQUIRED_STATIC_PROPERTIES = ["entry_point"]
 
-    def __init__(cls: Any, name: str, bases: List[Any], attrs: List[Any]) -> None: # sourcery skip: instance-method-first-arg-name
-        
-
+    def __init__(
+        cls: Any, name: str, bases: List[Any], attrs: List[Any]
+    ) -> None:  # sourcery skip: instance-method-first-arg-name
         if not hasattr(cls, "plugins"):
             # This branch only executes when processing the mount point itself.
             # So, since this is a new plugin type, not an implementation, this
@@ -20,19 +28,25 @@ class PluginMount(ABCMeta):
             # This must be a plugin implementation, which should be registered.
             # Simply appending it to the list is all that's needed to keep
             # track of it later.
+            cls._verify()
             cls.plugins[cls.__name__] = cls
-    @classmethod
-    def _verify_attributes(cls):
-        for base in set(cls.__mro__):
-            if not_implemented := set(cls.REQUIRED_STATIC_PROPERTIES) - set(
-                base.__dict__.keys()
-            ):
-                raise NotImplementedError(
-                    f"Attribute(s) {not_implemented} not implemented by class {base.__name__}"
-                )
 
-    @classmethod
-    def load(cls):
-        cls._verify_attributes()
-        for entry_point in pkg_resources.iter_entry_points(cls.entry_point):
-            entry_point.load()
+    def _verify(cls):  # sourcery skip: instance-method-first-arg-name
+        base = cls.__mro__[-2]
+        if not_implemented := set(cls.REQUIRED_STATIC_PROPERTIES) - set(base.__dict__.keys()):
+            raise NotImplementedError(
+                f"Attribute(s) {not_implemented} not implemented by class {base.__name__}"
+            )
+
+    def load(cls):  # sourcery skip: instance-method-first-arg-name
+        if cls == PluginMount:
+            return None
+
+        eps = (
+            pkg_resources.iter_entry_points(cls.entry_point)
+            if USE_LEGACY
+            else entry_points().select(group=cls.entry_point)
+        )
+
+        for ep in eps:
+            ep.load()
